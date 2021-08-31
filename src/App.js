f31287b803d08a23bf59035c13a24e28880620dd
apucheta <aguspucheta24293@gmail.com>
2021-08-30 22:14:17 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
+import AnimalCard from './component/animal-card';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <AnimalCard></AnimalCard>
+        <AnimalCard></AnimalCard>
+        <AnimalCard></AnimalCard>
+        <AnimalCard></AnimalCard>
+        <AnimalCard></AnimalCard>
+        <AnimalCard></AnimalCard>
+      </div>
     </div>
   );
 }
