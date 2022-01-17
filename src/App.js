317202673955329645a244d2bedab8d6e5208c09
apucheta <aguspucheta24293@gmail.com>
2022-01-17 20:03:02 -0300

    clase ruteo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import PageHome from './home-page.js';
-import PageArtist from './page-artist.js';
-
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import PageHome from "./home-page.js";
+import PageArtist from "./page-artist.js";
+import PageSearchResult from "./page-search-result.js";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
-    return <PageArtist />;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/busqueda" component={PageSearchResult} />
+          <Route exact path="/artista" component={PageArtist} />
+          <Route path="/" component={PageHome} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
